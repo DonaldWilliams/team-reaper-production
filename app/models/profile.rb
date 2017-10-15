05960698b94202544bf07433ce81dc2b0ca7e0da
phantomfreeJr <vdavis@siu.edu>
2017-10-15 00:30:33 -0500

    fix user profile null issue

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,3 @@
 class Profile < ApplicationRecord
   belongs_to :users
-
-  #validates_presence_of :pet, :pet_type ,:quiet, :noisy, :clean, :messy, :kind_of_clean,
-  #                      :party, :drink, :smoke, :study, :major
 end
