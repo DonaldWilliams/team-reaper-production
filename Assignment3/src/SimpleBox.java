bb64baa8c19bbbb4ce222403dc3910c1c6002ef4
Angel Galeana <angel.galeanajr@siu.edu>
2017-10-31 21:44:40 -0500

    Java program

---
+++ Assignment3/src/SimpleBox.java
@@ -0,0 +1,27 @@
+
+public class SimpleBox {
+
+	public boolean isEmpty() {
+		return false;
+		
+	}
+	
+	public boolean isFull() {
+		return false;
+		
+	}
+	
+	public void push(long j) {
+	     
+	   }
+    public long pop() {
+		return 0;
+	      
+	   }
+    public long peek() {
+		return 0;
+	       
+	   }
+
+	
+}
